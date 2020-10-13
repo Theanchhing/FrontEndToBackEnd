45c911d08e12ef957cc09bd11031b71bb884de79
Theanchhing <taingtheanchhing18@kit.edu.kh>
2020-10-13 14:34:34 +0700

    initializing

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="app">
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+      style="display: block; margin: 0 auto;"
+    />
+    <router-view />
+  </div>
+</template>
